d7ba6cb79fa0f2790f02501cdaa27beb082b429b
Zoro444 <zoro.simonyan1995@gmail.com>
2022-12-12 21:17:31 +0400

    add time complexity

--- 11.12.2022/leetcode-724.js
+++ 11.12.2022/leetcode-724.js
@@ -1,23 +1,29 @@
 let pivotIndex = function(nums) {
-    if (nums.length === 1) return 0;
-    let x = 0; y = 0;
+    if (nums.length === 1) return 0; //O(1)
+    let x = 0; //O(1)
+    let y = 0; //O(1) 
    
-    for (let i = 1; i < nums.length; i++) {
-        x += nums[i];
+    for (let i = 1; i < nums.length; i++) { //O(n)
+        x += nums[i]; //O(1)
     }
 
     for (let i = 1; i < nums.length; i++){
-        if (x - y === 0) { 
-           if (i === 1) return 0; 
-            return i;
+        if (x - y === 0) { //O(1)
+           if (i === 1) return 0; //O(1)
+            return i; //O(1)
         }
         
-        y += nums[i - 1];
-        x -= nums[i];
+        y += nums[i - 1]; //O(1)
+        x -= nums[i]; //O(1)
 
-        if (i === nums.length - 1 && x - y !== 0 ) return -1;
-        if (x - y === 0) return i;
+        if (i === nums.length - 1 && x - y !== 0 ) return -1; //O(1)
+        if (x - y === 0) return i; //O(1)
     }
-};
+}; //O(1 + 1 + 1 + n + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 ) = O(11 + n) = O(n)
 
-console.log(pivotIndex([-1,-1,0,1,1,0]));
+/* 
+    Big O rules
+    1.constants rule: remove constants
+    2.sum rule: find the fastest growing term
+    3.product rule: take out the coefficient 
+*/
