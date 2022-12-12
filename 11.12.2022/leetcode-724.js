33fbeb91494a7fe1cb9f22443fb6950ff636dd3c
Zoro444 <zoro.simonyan1995@gmail.com>
2022-12-12 21:24:41 +0400

    fix time complexity

--- 11.12.2022/leetcode-724.js
+++ 11.12.2022/leetcode-724.js
@@ -4,13 +4,13 @@ let pivotIndex = function(nums) {
     let y = 0; //O(1) 
    
     for (let i = 1; i < nums.length; i++) { //O(n)
-        x += nums[i]; //O(1)
+        x += nums[i]; //O(n)
     }
 
     for (let i = 1; i < nums.length; i++){ //O(n)
-        if (x - y === 0) { //O(1)
-           if (i === 1) return 0; //O(1)
-            return i; //O(1)
+        if (x - y === 0) { //O(n)
+           if (i === 1) return 0; //O(n)
+            return i; //O(n) 
         }
         
         y += nums[i - 1]; //O(1)
@@ -19,7 +19,7 @@ let pivotIndex = function(nums) {
         if (i === nums.length - 1 && x - y !== 0 ) return -1; //O(1)
         if (x - y === 0) return i; //O(1)
     }
-}; //O(1 + 1 + 1 + n + 1 + n + 1 + 1 + 1 + 1 + 1 + 1 + 1 ) = O(11 + 2n) = O(n)
+}; //O(1 + 1 + 1 + n + n + n + n + n + n + 1 + 1 + 1 + 1 ) = O(7 + 6n) = O(n)
 
 /* 
     Big O rules
