28993460b9c91d7836192458000e15718b4faa7e
Zoro444 <zoro.simonyan1995@gmail.com>
2022-12-14 03:42:22 +0400

    fix

--- 13.12.2022/bitwise-problems/count-of-1.js
+++ 13.12.2022/bitwise-problems/count-of-1.js
@@ -5,7 +5,9 @@
 function countOfOne(dec) {
   let count = 0;
   for (let i = 0 ; i < 32; i++) { 
-    if (1 << i & dec) count++;
+    if (1 << i & dec) {
+      count++;
+    }
   } 
   return count;
 } /* time complexity = O(1) space complexity = O(1) */
--- 13.12.2022/bitwise-problems/is-count-of-1-even.js
+++ 13.12.2022/bitwise-problems/is-count-of-1-even.js
@@ -5,7 +5,9 @@
 function countOfOne(dec) {
   let count = 0;
   for (let i = 0 ; i < 32; i++) { 
-    if (1 << i & dec) count++;
+    if (1 << i & dec) {
+      count++;
+    }
   } 
   return count % 2 === 0;
 } /* time complexity = O(1) space complexity = O(1) */
