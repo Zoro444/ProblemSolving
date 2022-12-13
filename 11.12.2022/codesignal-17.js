dc4c8e59aba4b49b4e987f50d09825de0a776402
Zoro444 <zoro.simonyan1995@gmail.com>
2022-12-13 13:25:31 +0400

    fix solution

--- 11.12.2022/codesignal-17.js
+++ 11.12.2022/codesignal-17.js
@@ -1,7 +1,3 @@
 function solution(n, k) {
-  return  parseInt((n >>> 0).toString(2)
-          .split('')
-          .map((v, i, arr) => { if (arr.length - k === i) v = 0;return v; })
-          .reduce((aggr, v) => aggr += v, ''), 2
-          );
+  return  n & ~(n << k - 1);       
 };
