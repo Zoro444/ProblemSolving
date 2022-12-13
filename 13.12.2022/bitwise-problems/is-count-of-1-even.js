f20495ba04fd6a9e66546b2928e02108b8742fb5
Zoro444 <zoro.simonyan1995@gmail.com>
2022-12-14 03:37:18 +0400

    fix spaces and add .gitignor

--- 13.12.2022/bitwise-problems/is-count-of-1-even.js
+++ 13.12.2022/bitwise-problems/is-count-of-1-even.js
@@ -7,5 +7,5 @@ function countOfOne(dec) {
   for (let i = 0 ; i < 32; i++) { 
     if (1 << i & dec) count++;
   } 
-    return count % 2 === 0  
+  return count % 2 === 0;
 } /* time complexity = O(1) space complexity = O(1) */
--- 13.12.2022/bitwise-problems/swap.js
+++ 13.12.2022/bitwise-problems/swap.js
@@ -3,16 +3,19 @@
 */
 
 function countOfOne(dec, i ,j) {
-    let x = 0;
-    let y = 0;
-    
-    1 << i - 1 & dec? x = 1 : x = 0;
-    1 << j - 1 & dec? y = 1 : y = 0;
+  let x = 0;
+  let y = 0;
 
-    if (x === y)  return dec;
-    else {
-      dec ^= (1 << i - 1)
-      dec ^= (1 << j - 1)
-      return dec;
-    }
-} /* time complexity = O(1) space complexity = O(1) */
+  1 << i - 1 & dec? x = 1 : x = 0;
+  1 << j - 1 & dec? y = 1 : y = 0;
+
+  if (x === y) {
+    return dec;
+  } 
+
+  dec ^= (1 << i - 1)
+  dec ^= (1 << j - 1)
+
+  return dec;
+}
+ /* time complexity = O(1) space complexity = O(1) */
