f7f8d656d62037bf0978b0c8595dfab13abf4057
Zoro444 <zoro.simonyan1995@gmail.com>
2022-12-14 12:37:02 +0400

    fix

--- 13.12.2022/leetcode-125.js
+++ 13.12.2022/leetcode-125.js
@@ -8,13 +8,19 @@
 
 let isPalindrome = function(s) {
     let str = ''; //space O(1)
-    s = s.toLowerCase(); //time O(n)
     
     for (let i = 0; i < s.length; i++) { //time O(n)
       if (s[i].charCodeAt() >= 48 && s[i].charCodeAt() <= 57 
        || s[i].charCodeAt() >= 65 && s[i].charCodeAt() <= 90 
        || s[i].charCodeAt() >= 97 && s[i].charCodeAt() <= 122
-       ) str += s[i]; //space O(n)
+       ) { 
+        if (s[i].charCodeAt() >= 65 && s[i].charCodeAt() <= 90 ) {
+            str += String.fromCodePoint(s[i].charCodeAt() + 32); 
+        }
+        else {
+            str += s[i]; //space O(n)
+        }
+      }
     }
 
     for (let i = 0, j = str.length - 1; i < Math.floor(str.length / 2); i++, j--) { //time O(n/2)
