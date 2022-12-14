78a636ad62f749bc12cc6ac4bed77b811887a73e
Zoro444 <zoro.simonyan1995@gmail.com>
2022-12-15 00:44:47 +0400

    new file

---
+++ 14.11.2022/leetcode-83.js
@@ -0,0 +1,21 @@
+/*
+  Given the head of a sorted linked list,
+  delete all duplicates such that each element appears only once.
+  Return the linked list sorted as well.
+*/
+
+ function deleteDuplicates(head) { //time O(n) space O(n)
+    while (head !== null){
+      if (head.next !== null && head.next.val !== head.val) {
+        head.next = deleteDuplicates(head.next);  
+        return head;
+      }
+      if (head.next !== null) {
+        head = head.next;  
+      }
+      else {
+        return head;
+      }
+    }
+    return head;
+}; /* time complexity = O(n) space complexity = O(n) */
