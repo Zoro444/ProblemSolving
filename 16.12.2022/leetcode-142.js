0115f7822411c9f8649a0f2329a739393b28c583
Zoro444 <zoro.simonyan1995@gmail.com>
2022-12-16 12:17:30 +0400

    add leetcode-147

---
+++ 16.12.2022/leetcode-142.js
@@ -0,0 +1,22 @@
+/*
+ Given head, the head of a linked list,
+ determine if the linked list has a cycle in it.
+*/
+
+function hasCycle(head) {
+  while (head) {
+    if (head.val !== undefined) {
+      head.val = undefined;
+    }
+    else {
+      return true;
+    }
+      if (head.next) {
+        head = head.next;
+      }
+    else { 
+      return false;
+    }
+  }
+  return false;
+};/*time complexity = O(n) space complexity = O(1) */ 
