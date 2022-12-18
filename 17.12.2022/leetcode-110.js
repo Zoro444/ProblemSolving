7d7a0b944730f878e142ceba5a90d3b2bc4de04f
Zoro444 <zoro.simonyan1995@gmail.com>
2022-12-18 11:57:26 +0400

    add leetcode-110

---
+++ 17.12.2022/leetcode-110.js
@@ -0,0 +1,51 @@
+/*
+ Given a binary tree, determine if it is 
+ height-balanced
+*/
+
+function isBalanced(root) {
+    let rootLeftDept = 0;
+    let rootRightDepth = 0; 
+    let balanced = true; 
+    
+    if (root) {
+        if (root.left){ 
+          rootLeftDept = recursion(root.left);// time O(n / 2)
+          if (!balanced) return false;
+        }
+     
+        if (root.right) { 
+          rootRightDepth = recursion(root.right); // time O(n / 2)
+          if (!balanced) return false;
+        }
+    }
+
+    if (rootLeftDept - rootRightDepth > 1 || rootRightDepth - rootLeftDept > 1) {
+        balanced = false;
+    }
+ 
+  function recursion(node) {
+    if (!balanced) return false;
+
+    let nodeLeftDepth = 0;
+    let nodeRightDepth = 0;
+    
+    if (node.left) {
+        nodeLeftDepth = recursion(node.left);
+        if (!balanced) return false;
+    } 
+
+    if (node.right) {
+        nodeRightDepth = recursion(node.right);
+        if (!balanced) return false;
+    }
+
+    if (nodeLeftDepth - nodeRightDepth > 1 || nodeRightDepth - nodeLeftDepth > 1) {
+        balanced = false;
+    }
+    nodeLeftDepth++; nodeRightDepth++;
+    return nodeLeftDepth > nodeRightDepth ? nodeLeftDepth : nodeRightDepth;
+  };
+  return balanced;
+}; /* time complexity = O(n / 2 + n / 2) = O(n) space complexity = O(1) */ 
+ 
